2c91e29a8341055e0d69c381af40c53fb6ae1053
raviranjan940 <raviranjandav940@gmail.com>
2024-08-24 16:38:58 +0530

    Form Structure Updated

---
+++ src/components/failure/failure.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function failure() {
+  return (
+    <div className='flex justify-center items-center text-center'>
+        You payment has been cancelled
+    </div>
+  )
+}
+
+export default failure
---
+++ src/components/success/success.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function success() {
+  return (
+    <div className='flex justify-center items-center text-center'>
+        You payment has been done successfully
+    </div>
+  )
+}
+
+export default success
