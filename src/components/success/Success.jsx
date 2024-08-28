a0ee854dd53fd31bf58178c5cc55c33de992a4c1
raviranjan940 <raviranjandav940@gmail.com>
2024-08-28 20:11:08 +0530

    Latest Updates Added

--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function Success() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-teal-50 text-teal-800">
+      {/* Success Page */}
       <h1 className="text-4xl font-bold mb-4">Payment Successful!</h1>
       <p className="text-lg text-center mb-8 max-w-lg">
         Thank you for your payment. Your transaction was completed successfully.
