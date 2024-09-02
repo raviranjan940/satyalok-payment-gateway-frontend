17ee31c2998ede1e07ddea7794e9d197178031ba
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-09-02 20:37:07 +0530

    refactor: Improve loading message in Loading component

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -64,7 +64,6 @@ function Loading() {
 
             <p className="text-black text-md">
                 {message}
-                {/* {dots} */}
             </p>
 
             <p className="text-xs mt-1 font-light">
