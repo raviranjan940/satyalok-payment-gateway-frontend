4227235711c2cfcc38782f89404e30d495499b4f
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-09-09 01:40:54 +0530

    Update Payment component to display maintenance mode message

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Field from "./Field"; // Import the Field component
 import bgImage from "../../assets/images/image_3.png";
@@ -6,6 +6,7 @@ import logo from "../../assets/logo_white.png";
 import Header from "../header/Header";
 import Loading from "../loading/Loading";
 import ErrorCard from "../error/ErrorCard";
+import { isMaintainanceMode } from "../../../config";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -127,6 +128,80 @@ function Payment() {
             setLoading(false);
         }
     };
+
+    if (isMaintainanceMode) {
+        const [hour, setHour] = useState(0);
+        const [minute, setMinute] = useState(0);
+        const [second, setSecond] = useState(0);
+
+        const currentTime = new Date();
+        const targetTime = new Date();
+        targetTime.setHours(currentTime.getHours() + 1);
+        targetTime.setMinutes(0);
+        targetTime.setSeconds(0);
+
+        useEffect(() => {
+            const interval = setInterval(() => {
+                const diff = targetTime - new Date();
+                if (diff <= 0) {
+                    setHour(1);
+                    setMinute(0);
+                    setSecond(0);
+                } else {
+                    const hours = Math.floor(diff / 1000 / 60 / 60);
+                    const minutes = Math.floor(diff / 1000 / 60) % 60;
+                    const seconds = Math.floor(diff / 1000) % 60;
+
+                    setHour(hours);
+                    setMinute(minutes);
+                    setSecond(seconds);
+                }
+            }, 1000);
+
+            return () => clearInterval(interval);
+        }, []);
+
+        return (
+            <>
+                <Header />
+                <div className="flex flex-col items-center justify-center h-screen">
+                    <h1 className="text-3xl font-bold text-red-500">
+                        Donation Site is Under Maintainance
+                    </h1>
+
+                    <div className="flex flex-col items-center justify-center mt-4">
+                        <div className="flex items-center gap-2">
+                            <div className="flex flex-col items-center">
+                                <span className="text-3xl font-bold text-black">
+                                    {hour < 10 ? `0${hour}` : hour}
+                                </span>
+                            </div>
+                            <span className="text-3xl font-bold text-black">
+                                :
+                            </span>
+                            <div className="flex flex-col items-center">
+                                <span className="text-3xl font-bold text-black">
+                                    {minute < 10 ? `0${minute}` : minute}
+                                </span>
+                            </div>
+                            <span className="text-3xl font-bold text-black">
+                                :
+                            </span>
+                            <div className="flex flex-col items-center">
+                                <span className="text-3xl font-bold text-black">
+                                    {second < 10 ? `0${second}` : second}
+                                </span>
+                            </div>
+                        </div>
+
+                        <p className="text text-red-500 mt-2">
+                            Kindly Check Back Later
+                        </p>
+                    </div>
+                </div>
+            </>
+        );
+    }
 
     return (
         <div className="">
