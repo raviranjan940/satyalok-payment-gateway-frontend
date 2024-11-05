fedd89044e0f10abff809353d8dcde20ab105de4
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-11-05 21:33:18 +0530

    Add NotFound component and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Payment from './components/payment/Payment';
-import Status from './components/status/Status';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Payment from "./components/payment/Payment";
+import Status from "./components/status/Status";
+import NotFound from "./components/notFound/NotFound";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Payment />} />
-        <Route path="/status/:id" element={<Status />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Payment />} />
+                <Route path="/status/:id" element={<Status />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/notFound/NotFound.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import image from "../../assets/card1.png";
+
+function NotFound() {
+    return (
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+            <img
+                src={image}
+                alt="404 Not Found"
+                className="absolute top-0 left-0 right-0 bottom-0 m-auto w-full h-full object-cover object-bottom -z-10"
+            />
+
+            <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+                <h1 className="text-4xl font-bold text-gray-800">
+                    404 Not Found
+                </h1>
+                <p className="text-gray-600">
+                    The page you are looking for does not exist.
+                </p>
+
+                <a href="/" className="text-blue-500 hover:underline">
+                    Go back to the home page
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;
